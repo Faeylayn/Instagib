3c5052b0fabfdb0af58d7647c738174d46a61e62
generic <generic@generic-pc.(none)>
2015-03-07 21:46:21 -0500

    First Proposal Final Project

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,29 @@
+class PicturesController < ApplicationController
+    def show
+
+    end
+
+    def index
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
